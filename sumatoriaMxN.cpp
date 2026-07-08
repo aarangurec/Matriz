a09feb03763f7952b936794e1bf0eeb4ec1d2711
aarangurec <aarangurec@unjbg.edu.pe>
2026-07-08 09:55:17 -0500

    Ejercicio Sumatoria MxN

--- sumatoriaMxN.cpp
+++ sumatoriaMxN.cpp
@@ -4,16 +4,17 @@ int main(){
 	int A[100][100];
 	int n;
 	cin>>n;
+	int m;
 	cin>>m;
 	for (int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
+		for (int j=0; j<m; j++){
 			cout<<"Ingrese el valor en "<<i+1<<j+1<<": ";
 			cin>>A[i][j];
 		}
 	}
 	int cont=0;
 	for(int i=0; i<n; i++){
-		for (int j=0; j<n; j++){
+		for (int j=0; j<m; j++){
 			cont=cont+A[i][j];}
 	} 
 	cout<<cont;
